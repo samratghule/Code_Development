71376cf17dccf6790a36c0dceb739c9a8864caf5
samratghule <samrat.ghule@gmail.com>
2021-02-09 00:05:45 +0530

    deleted main.c file in cpp project ande deleted debug folder

--- CPP_codes/winapi_file_handling/main.cpp
+++ CPP_codes/winapi_file_handling/main.cpp
@@ -35,5 +35,5 @@ int main()
    // Close the handle once we don't need it.
    CloseHandle(hFile);
    system("PAUSE");
-   return 0;
+D:\GIT\Codeblocks\CPP_codes\winapi_file_handling\main.cpp   return 0;
 }
