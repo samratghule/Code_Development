f59890afac4df7c097a57147e284fbcee1090839
samratghule <samrat.ghule@gmail.com>
2021-02-09 00:26:38 +0530

    added data_types project in c_codes folder

---
+++ C_codes/Data_types/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+
+    printf("Size of integer:        %d\n", sizeof(int));
+    printf("Size of character:      %d\n",sizeof(char));
+    printf("Size of float:          %d\n",sizeof(float));
+
+    printf("Size of unsigned int:   %d\n",sizeof(unsigned int));
+    printf("Size of signed int:     %d\n",sizeof(signed int));
+    printf("Size of short int:      %d\n",sizeof(short int));
+    printf("Size of long int:       %d\n",sizeof(long int));
+    printf("Size of long long int:  %d\n",sizeof(long long int));
+    return 0;
+}
