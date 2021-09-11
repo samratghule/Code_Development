d0b3122da2abdd48f0f9da39eb1f186804943f0f
Samrat <samrat.ghule@gmail.com>
2021-09-11 11:34:16 +0530

    changed array index text

--- C_codes/Arrays/Array_ Search_Algorithm/main.c
+++ C_codes/Arrays/Array_ Search_Algorithm/main.c
@@ -5,8 +5,8 @@ int main()
 {
     struct ArrayADT Array={{1,2,23,4,5},10,5};
     display(Array);
-    printf("key is at index %d\n",linearsearch(Array ,7));
-    printf("key is at index %d\n",binarysearch(Array ,23));
+    printf("key is at index location %d\n",linearsearch(Array ,7));
+    printf("key is at index location %d\n",binarysearch(Array ,23));
 
     return 0;
 }
